b368aad2361d8b199f6719b089e95fc2a16810d2
SeanBrad <sbsimmo@gmail.com>
2014-06-02 21:28:45 -0500

    Finally got it

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+
 	$('.ryu').mouseenter(function(){
+		console.log('click');
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
+		console.log('leave');
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
@@ -25,6 +28,26 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	$(document).keydown(function(event) {
+		console.log('x key');
+		if (event.which == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if (event.which == 88) {
+			if ($('.ryu').is(':hover')) {
+				$('.ryu-ready').show();
+				$('.ryu-cool').hide();
+			} else {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+		}
+	});
+	
 });
 
 function playHadouken () {
